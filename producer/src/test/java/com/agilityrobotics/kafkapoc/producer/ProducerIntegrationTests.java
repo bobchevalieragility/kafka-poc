03a9bdd8d7368a374a5ca1b603f11f9f7fbd8f6e
Bob Chevalier <bob.chevalier@agilityrobotics.com>
2025-07-25 09:35:08 -0400

    Use KafkaTestUtils

--- producer/src/test/java/com/agilityrobotics/kafkapoc/producer/ProducerIntegrationTests.java
+++ producer/src/test/java/com/agilityrobotics/kafkapoc/producer/ProducerIntegrationTests.java
@@ -4,7 +4,7 @@ import com.agilityrobotics.kafkapoc.common.kafka.ArcEventProducer;
 import com.agilityrobotics.kafkapoc.models.arcevents.ArcEvent;
 import com.agilityrobotics.kafkapoc.models.arcevents.ShiftStart;
 import com.google.protobuf.Timestamp;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,10 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.listener.ContainerProperties;
-import org.springframework.kafka.listener.KafkaMessageListenerContainer;
-import org.springframework.kafka.listener.MessageListener;
-import org.springframework.kafka.test.utils.ContainerTestUtils;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -27,10 +24,7 @@ import org.testcontainers.utility.DockerImageName;
 import org.testcontainers.utility.MountableFile;
 
 import java.io.IOException;
-import java.util.Map;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.TimeUnit;
+import java.util.List;
 
 @SpringBootTest
 @DirtiesContext
@@ -40,6 +34,7 @@ class ProducerIntegrationTests {
   @Container
   static KafkaContainer kafkaContainer = new KafkaContainer(DockerImageName.parse("apache/kafka-native:3.9.1"));
 
+  @SuppressWarnings("resource")
   @Container
   static GenericContainer<?> schemaRegistryContainer = new GenericContainer<>(
       DockerImageName.parse("motoserver/moto"))
@@ -63,7 +58,7 @@ class ProducerIntegrationTests {
     registry.add("aws.schema.registry.endpoint", () -> endpointUrl);
     registry.add("spring.kafka.bootstrap-servers", () -> kafkaContainer.getBootstrapServers());
 
-    // Add consumer properties for test consumer
+    // Add consumer properties for mock consumer
     registry.add("spring.kafka.consumer.group-id", () -> "foo");
     registry.add("spring.kafka.consumer.auto-offset-reset", () -> "earliest");
     registry.add("spring.kafka.consumer.key-deserializer",
@@ -78,9 +73,6 @@ class ProducerIntegrationTests {
   @Autowired
   private ConsumerFactory<String, ArcEvent> consumerFactory;
 
-  private KafkaMessageListenerContainer<String, ArcEvent> listener;
-  private BlockingQueue<ConsumerRecord<String, ArcEvent>> messages;
-
   @BeforeAll
   static void beforeAll() {
     // Allow the Kafka client to interact with AWS Glue Schema Registry
@@ -93,17 +85,6 @@ class ProducerIntegrationTests {
     // Create a registry named 'arc-registry' in the Glue Schema Registry container
     schemaRegistryContainer.execInContainer("chmod", "+x", "/moto/moto_server_init");
     schemaRegistryContainer.execInContainer("/moto/moto_server_init");
-
-    messages = new LinkedBlockingQueue<>();
-
-    Map<String, Object> consumerProps = consumerFactory.getConfigurationProperties();
-    ContainerProperties containerProps = new ContainerProperties("arc-events");
-    containerProps.setGroupId((String) consumerProps.get(ConsumerConfig.GROUP_ID_CONFIG));
-    containerProps.setPollTimeout(100);
-    listener = new KafkaMessageListenerContainer<>(consumerFactory, containerProps);
-    listener.setupMessageListener((MessageListener<String, ArcEvent>) messages::add);
-    listener.start();
-    ContainerTestUtils.waitForAssignment(listener, 1);
   }
 
   @Test
@@ -114,11 +95,13 @@ class ProducerIntegrationTests {
         .setNanos((int) ((millis % 1000) * 1000000)).build();
     ArcEvent event = ArcEvent.newBuilder().setId("123").setEventTime(timestamp).setShiftStart(shiftEvent).build();
 
-    producer.publish("arc-events", event);
+    String topic = "arc-events";
+    producer.publish(topic, event);
 
-    ArcEvent consumedEvent = messages.poll(5, TimeUnit.SECONDS).value();
-    System.out.println(consumedEvent.toString());
-    listener.stop();
+    Consumer<String, ArcEvent> consumer = consumerFactory.createConsumer();
+    consumer.subscribe(List.of(topic));
+    ConsumerRecord<String, ArcEvent> rec = KafkaTestUtils.getSingleRecord(consumer, topic);
+    System.out.println(rec.value().toString());
   }
 
 }
